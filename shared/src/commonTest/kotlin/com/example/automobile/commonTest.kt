d84bf9652baa4c9b459f1c452d75360784f12d12
Stefaneco <piotrkalin2@gmail.com>
2022-11-24 11:34:29 +0100

    Adding files in project 'AutoMobile' to Git...

---
+++ shared/src/commonTest/kotlin/com/example/automobile/commonTest.kt
@@ -0,0 +1,12 @@
+package com.example.automobile
+
+import kotlin.test.Test
+import kotlin.test.assertTrue
+
+class CommonGreetingTest {
+
+    @Test
+    fun testExample() {
+        assertTrue(Greeting().greeting().contains("Hello"), "Check 'Hello' is mentioned")
+    }
+}
